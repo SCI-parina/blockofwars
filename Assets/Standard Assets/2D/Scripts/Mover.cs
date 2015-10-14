56b97f681bb1244a82190c03df1416872dcfbb40
Aleksi Asikainen <aleksi.asikainen@aalto.fi>
2015-10-14 14:35:05 +0300

    Initial commit with a sample project that has 2 moving blocks which destroy eachother

---
+++ Assets/Standard Assets/2D/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float MoveSpeed = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x +( MoveSpeed* Time.deltaTime), transform.position.y, transform.position.z);
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
