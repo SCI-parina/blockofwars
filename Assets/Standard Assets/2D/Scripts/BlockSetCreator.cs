2caedd1822ff2f770d55a167cfb52bd3cd64900f
htoivan <hegez1994@gmail.com>
2015-10-31 01:43:01 +0200

    Added basic block functionality and spawning

---
+++ Assets/Standard Assets/2D/Scripts/BlockSetCreator.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSetCreator : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject[,] blocks;
+	private int LastBlocks;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void RealStart (bool[,] blockArray, float moveSpeed) {
+		// blockArray should be 5x5
+		blocks = new GameObject[5, 5];
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if(blockArray[i, j]){
+					Vector3 newPos = transform.position + new Vector3(i*block.GetComponent<Renderer>().bounds.size.x, j*block.GetComponent<Renderer>().bounds.size.y, 0);
+					GameObject b = (GameObject)Instantiate(block, newPos, Quaternion.identity);
+					b.GetComponent<Mover>().MoveSpeed = moveSpeed;
+					blocks[i, j] = b;
+				}
+			}
+		}
+		LastBlocks = BlockAmount();
+	}
+	
+	int BlockAmount () {
+		int amount = 0;
+		foreach (GameObject o in blocks) {
+			if (o != null) amount++;
+		}
+		return amount;
+	}
+	
+	bool IsDestroyed () {
+		if(BlockAmount() == 0) return true;
+		ArrayList visited = new ArrayList();
+		bool found = false;
+		int size = BlockAmount();
+		int x = 0;
+		int y = 0;
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if(!found && blocks[i, j] != null){
+					x = i;
+					y = j;
+					found = true;
+				}
+			}
+		}
+
+		visited = findConnected(visited, x, y);
+
+		if (visited.Count == size) return false;
+		else return true;
+	}
+
+	bool wasVisited(ArrayList visited, int x, int y) {
+		bool found = false;
+		foreach (int[] i in visited) {
+			if (i[0] == x && i[1] == y) found = true;
+		}
+		return found;
+	}
+
+	ArrayList findConnected(ArrayList visited, int x, int y) {
+		visited.Add (new int[] {x, y});
+		int[] next;
+		int i = 0;
+		ArrayList n = neighbours(x, y);
+		while (i < n.Count) {
+			next = (int[])n[i];
+			if(blocks[next[0], next[1]] != null && !wasVisited(visited, next[0], next[1])){
+				visited = findConnected(visited, next[0], next[1]);
+			}
+			i++;
+		}
+		return visited;
+	}
+
+
+	ArrayList neighbours(int x, int y) {
+		ArrayList n = new ArrayList();
+		if(x > 0 && blocks[x-1, y] != null) n.Add (new int[] {x-1, y});
+		if(y > 0 && blocks[x, y-1] != null) n.Add (new int[] {x, y-1});
+		if(x < 4 && blocks[x+1, y] != null) n.Add (new int[] {x+1, y});
+		if(y < 4 && blocks[x, y+1] != null) n.Add (new int[] {x, y+1});
+		return n;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int BlocksRemaining = BlockAmount();
+		if (BlocksRemaining < LastBlocks) {
+			LastBlocks = BlocksRemaining;
+			if(IsDestroyed()){
+				//Kill all
+				foreach (GameObject o in blocks) {
+					if (o != null){
+						Destroy(o);
+					}
+				}
+				Destroy(gameObject);
+			}
+		}
+		
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/ExampleBlockSpawner.cs
@@ -0,0 +1,85 @@
+﻿/*
+ *Spawns blocks when keys 1-6 are pressed. 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class ExampleBlockSpawner : MonoBehaviour {
+
+	bool[,] Block1, Block2, Block3;
+	public GameObject blockSet;
+	public Vector3 Team1 = new Vector3(-5, 0, 0);
+	public Vector3 Team2 = new Vector3(5, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		//Some blocks
+		Block1 = new bool[5, 5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				Block1[i, j] = false;
+			}
+		}
+		Block1[4, 0] = true;
+		Block1[4, 1] = true;
+		Block1[4, 3] = true;
+		Block1[4, 2] = true;
+		Block1[4, 4] = true;
+		Block1[3, 2] = true;
+
+		Block2 = new bool[5, 5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j == 2) Block2[i, j] = true;
+				else Block2[i, j] = false;
+			}
+		}
+
+		Block3 = new bool[5, 5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j == 2 || j == 4) Block3[i, j] = true;
+				else Block3[i, j] = false;
+			}
+		}
+		Block3[2, 3] = true;
+		Block3[0, 3] = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown("1")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team1, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block1, 1f);
+		}
+		if (Input.GetKeyDown("2")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team1, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block2, 1f);
+		}
+		if (Input.GetKeyDown("3")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team1, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block3, 1f);
+		}
+		if (Input.GetKeyDown("4")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team2, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block1, -1f);
+		}
+		if (Input.GetKeyDown("5")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team2, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block2, -1f);
+		}
+		if (Input.GetKeyDown("6")){
+			GameObject g = (GameObject)Instantiate(blockSet, Team2, Quaternion.identity);
+			g.GetComponent<BlockSetCreator>().RealStart(Block3, -1f);
+		}
+	}
+}
